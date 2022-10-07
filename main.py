ab7c459bbfce7484655291e72acd6ac113cae30a
236493 Banachowicz Anna <236493@edu.p.lodz.pl>
2022-10-07 20:38:16 +0200

    created createSheep function and called it in main

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from project.factories.SheepFactory import SheepFactory
 from project.model.sheep import Sheep
 
 if __name__ == '__main__':
-    sample = Sheep(1, 2.0, 3.0)
-    print(sample.__str__())
+    # sample = Sheep(1, 2.0, 3.0)
+    # print(sample.__str__())
+
+    factory = SheepFactory(10)
+    factory.createSheep(5)
