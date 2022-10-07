269472610acdc67a4671b2024fd29c17ab763d8c
Damian2057 <ddamian2057@gmail.com>
2022-10-07 20:09:45 +0200

    GameSimulation

--- project/model/sheep.py
+++ project/model/sheep.py
@@ -1,16 +1,16 @@
 class Sheep:
     isAlive: bool
 
-    def __init__(self, id, x, y):
+    def __init__(self, id, coX, coY):
         self.id = id
-        self.x = x
-        self.y = y
+        self.coX = coX
+        self.coY = coY
         self.isAlive = True
 
     def __str__(self):
         return "Sheep[id= " + str(self.id) \
-               + ", x= " + str(self.x) \
-               + ", y= " + str(self.y) \
+               + ", coX= " + str(self.coX) \
+               + ", coY= " + str(self.coY) \
                + ", isAlive= " \
                + str(self.isAlive) \
                + "]"
