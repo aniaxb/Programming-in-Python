d7ef3f91e4c358a7c1899d3b872390c9a6c837ff
Damian2057 <ddamian2057@gmail.com>
2022-10-07 19:08:25 +0200

    prepare basic entities

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from project.model.sheep import sheep
+from project.model.sheep import Sheep
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-foo = sheep(1, 2.0, 3.0)
-print("foo.id = " + str(foo.id))
+    sample = Sheep(1, 2.0, 3.0)
+    print(sample.__str__())
--- project/model/wolf.py
+++ project/model/wolf.py
@@ -1,5 +1,14 @@
-class wolf:
-    def __init__(self, coX, coY):
-        self.coX = coX
-        self.coY = coY
+class Wolf:
+    sheep_counter: int
 
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.sheep_counter = 0
+
+    def __str__(self):
+        return "Wolf[" \
+               + ", x= " + str(self.x) \
+               + ", y= " + str(self.y) \
+               + ", kills= " + str(self.sheep_counter) \
+               + "]"
