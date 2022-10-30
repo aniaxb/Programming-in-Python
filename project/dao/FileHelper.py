94cfeaacd52850d21ddbf3b91aeda645495ec96c
Damiaan <ddamian2057@gmail.com>
2022-10-30 18:24:52 +0100

    fixed method saving to csv file

--- project/dao/FileHelper.py
+++ project/dao/FileHelper.py
@@ -3,6 +3,8 @@ import logging
 
 from project.exceptions.exceptions import file_exception
 
+filename = 'alive.csv'
+
 
 def save_csv(round_number, sheep_amount):
     try:
@@ -10,13 +12,13 @@ def save_csv(round_number, sheep_amount):
         column_titles = ['round', 'alive']
         if round_number == 1:
             logging.debug("Create a new file")
-            with open('alive.csv', mode='w', newline='') as file:
+            with open(filename, mode='w', newline='') as file:
                 file_writer = csv.DictWriter(file, fieldnames=column_titles)
                 file_writer.writeheader()
                 file_writer.writerow({'round': round_number, 'alive': sheep_amount})
         else:
-            with open('alive.csv', mode='a', newline='') as file:
+            with open(filename, mode='a', newline='') as file:
                 file_writer = csv.DictWriter(file, fieldnames=column_titles)
                 file_writer.writerow({'round': round_number, 'alive': sheep_amount})
-    except Exception:
+    except IOError:
         raise file_exception()
