4e0c7eb82249d0fccda21c26237bad8c5090f08d
Damiaan <ddamian2057@gmail.com>
2022-11-07 14:14:57 +0100

    bugfix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='chase',
-    version='0.0.1',
+    version='0.0.3',
     author='Damian&Ania',
     author_email='236503@edu.p.lodz.pl',
     packages=setuptools.find_packages(),
